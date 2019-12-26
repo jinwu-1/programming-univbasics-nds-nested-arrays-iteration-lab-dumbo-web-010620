f379ad75594fc4ae0210a16b4363e46a4272b294
Jin Wu <jinn.wuu@gmail.com>
2019-12-26 16:17:43 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -44,8 +44,7 @@ def total_even_pairs(src)
   total = 0
   while row_index < src.length do
     if row_index[0] % 2 == 0 && row_index[1] % 2 == 0
-      total += src[row_index][0]
-      total += src[row_index][1]
+      total = row_index[0] + row_index[1]
     end
   row_index += 1
   end
